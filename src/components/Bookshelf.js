59c61bc15fe98212d271e6b238ea2ec5eb6fd284
Matheus Bafutto <matheusbafutto@gmail.com>
2017-08-18 11:28:25 -0300

    refactor: Make static components of Bookshelf and Book
    Making bookshelf and book as two separate components to start improving code readability on App.js

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import * as BooksAPI from '../BooksAPI'
+import '../App.css'
+import Book from './Book'
+
+class Bookshelf extends Component {
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              <Book
+                title='To Kill a Mockingbird'
+                url='http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+                author='Harper Lee'
+              />
+            </li>
+            <li>
+              <Book
+              title='Enders Game'
+              url='http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
+              author='Orson Scott Card'
+              />
+            </li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Bookshelf;
