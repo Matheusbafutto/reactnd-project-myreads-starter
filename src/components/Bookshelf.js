79a72d46b558bbee9fc6b3e046dae850ada8572a
Matheus Bafutto <matheusbafutto@gmail.com>
2017-08-25 12:26:51 -0300

    refactor: replace static 'Want to Read' bookshelf for Bookshelf component
    Replace static 'Want to Read' for its component counter part is another step on making myReads application more dynamic.

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -26,12 +26,13 @@ class Bookshelf extends Component {
 
   renderBook = (book, i) => {
     return(
+      <li key={i}>
       <Book
-        key={i}
         title={book.title}
         previewLink={book.previewLink}
         author={book.author}
       />
+      </li>
     )
   }
 
@@ -50,24 +51,7 @@ class Bookshelf extends Component {
         <h2 className="bookshelf-title">{this.props.shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-
-            <li>
             { this.state.books.map(this.renderBook) }
-            </li>
-            <li>
-              <Book
-                title='To Kill a Mockingbird'
-                url='http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api'
-                author='Harper Lee'
-              />
-            </li>
-            <li>
-              <Book
-              title='Enders Game'
-              url='http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
-              author='Orson Scott Card'
-              />
-            </li>
           </ol>
         </div>
       </div>
