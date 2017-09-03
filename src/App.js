d2c527e4a1f5a791f06337d55ccf62d3c0e330ce
Matheus Bafutto <matheusbafutto@gmail.com>
2017-09-03 17:10:16 -0300

    refactor: change state managed routing to react-router-dom routing
    react-router-dom routes based on the url. This makes the app more compatible with the browser's native features. This could not be achieved through state management routing.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Bookshelf from './components/Bookshelf'
+import { Route, Link, BrowserRouter } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -11,15 +12,12 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: true,
     booksAvailable: [],
   }
 
-  handleBookStatusChange(newShelf) {
-    console.log(newShelf);
-    this.setState(state => {
+  handleBookShelfChange(newShelf, bookTitle) {
+    console.log(bookTitle);
 
-    })
   }
 
   componentDidMount() {
@@ -33,58 +31,62 @@ class BooksApp extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+      <BrowserRouter>
+        <div className="app">
+          <Route path='/search' exact render={() => (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <Link className="close-search" to='/'>Close</Link>
+                <div className="search-books-input-wrapper">
+                  {/*
+                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                    You can find these search terms here:
+                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                    you don't find a specific author or title. Every search is limited by search terms.
+                  */}
+                  <input type="text" placeholder="Search by title or author"/>
 
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid"></ol>
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf
-                  shelfName='Currently Reading'
-                  books={this.state.booksAvailable}
-                  onBookChange={this.handleBookStatusChange}
-                />
-                <Bookshelf
-                  shelfName='Want to Read'
-                  books={this.state.booksAvailable}
-                  onBookChange={this.handleBookStatusChange}
-                />
-                <Bookshelf
-                  shelfName='Read'
-                  books={this.state.booksAvailable}
-                  onBookChange={this.handleBookStatusChange}
-                />
+          )}/>
+
+          <Route path='/' exact render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Bookshelf
+                    shelfName='Currently Reading'
+                    books={this.state.booksAvailable}
+                    onBookChange={this.handleBookShelfChange}
+                  />
+                  <Bookshelf
+                    shelfName='Want to Read'
+                    books={this.state.booksAvailable}
+                    onBookChange={this.handleBookShelfChange}
+                  />
+                  <Bookshelf
+                    shelfName='Read'
+                    books={this.state.booksAvailable}
+                    onBookChange={this.handleBookShelfChange}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to='/search'>Add a book</Link>
               </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
+          )}/>
+        </div>
+      </BrowserRouter>
     )
   }
 }
