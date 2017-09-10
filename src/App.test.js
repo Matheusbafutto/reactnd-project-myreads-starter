c3e677ca592a1145caa8dbb2241be5733b14ea1d
Matheus Bafutto <matheusbafutto@gmail.com>
2017-09-10 16:42:01 -0300

    test: Start implementing tests with jest and enzyme
    Add a few tests to start TDD on myReads project

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
+/**
+ This course is not designed to teach Test Driven Development.
+ Feel free to use this file to test your application, but it
  is not required.
 **/
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  expect(shallow(<App />)).toMatchSnapshot();
 })
 
+it('mounts my component', () => {
+  const wrapper = shallow(
+    <div className="my-component">
+      <strong>Hello World!</strong>
+    </div>
+  );
 
+  expect(wrapper).toMatchSnapshot();
+});
---
+++ src/setupTests.js
@@ -0,0 +1,17 @@
+import { shallow, render, mount } from 'enzyme';
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+// Fail tests on any warning
+console.error = message => {
+       throw new Error(message);
+};
+
+const localStorageMock = {
+        getItem: jest.fn(),
+        setItem: jest.fn(),
+        clear: jest.fn()
+};
+
+global.localStorage = localStorageMock;
