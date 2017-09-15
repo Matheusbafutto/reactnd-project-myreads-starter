18a2c965c86eede3f62c46d72d0b1e4284aafbd1
Matheus Bafutto <matheusbafutto@gmail.com>
2017-09-15 13:54:13 -0300

    tests: Begin tests for Bookshelf component
    Not yet fully functional.

--- src/__tests__/Bookshelf.test.js
+++ src/__tests__/Bookshelf.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Bookshelf from '../components/Bookshelf'
 
-const onBookChange = () => {}
-const books = []
-
 it('renders without crashing', () => {
-  expect(shallow(<Bookshelf onBookChange={onBookChange} books={books} />)).toMatchSnapshot();
-})
+  expect(shallow(<Bookshelf shelfName='Read' onBookChange={onBookChange} books={books} />)).toMatchSnapshot();
+});
+
+it('matchBookToShelf method works properly',() => {
+  const BookshelfWrapper = shallow(<Bookshelf shelfName='Read' onBookChange={onBookChange} books={books} />);
+  expect(BookshelfWrapper.instance().matchBookToShelf(books)).toBe(true);
+});
