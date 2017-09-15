69841e5d5e27f404acf9727601adb05182e69506
Matheus Bafutto <matheusbafutto@gmail.com>
2017-09-15 11:35:52 -0300

    tests: update setupTests.js file with global mock Book atributes

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,6 @@
 import { shallow, render, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+global.toJson = toJson;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
@@ -7,6 +9,16 @@ global.mount = mount;
 console.error = message => {
        throw new Error(message);
 };
+
+
+const book = {
+  imageLinks: { thumbnail: 'mock' },
+  title: 'teste',
+  author: 'Matheus'
+}
+const onBookChange = jest.fn();
+global.book = book;
+global.onBookChange = onBookChange;
 
 const localStorageMock = {
         getItem: jest.fn(),
