fbdc6c54b787f009f3c49793f4da6a3bd0d30ea7
Matheus Bafutto <matheusbafutto@gmail.com>
2017-09-20 13:31:18 -0300

    tests: work on SearchPage test implementations
    Not yet finished.

--- src/__tests__/SearchPage.test.js
+++ src/__tests__/SearchPage.test.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import SearchPage from '../components/SearchPage'
+import { BrowserRouter } from 'react-router-dom'
 
 const onBookChange = () => {}
 const booksOnShelves = []
 
 it('renders without crashing', () => {
   expect(shallow(<SearchPage booksOnShelves={booksOnShelves} onBookChange={onBookChange} />)).toMatchSnapshot();
-})
+});
+
+it('change on searchbar trigers search handler',() => {
+  let searchPageWrapper = shallow(<BrowserRouter><SearchPage onBookChange={onBookChange} booksOnShelves={books}/></BrowserRouter>);
+
+  let dummyWrapper = searchPageWrapper.find(SearchPage).dive();
+  dummyWrapper.instance().handleChange = jest.fn((event) => { console.log('trigered'); } );
+  dummyWrapper.update();
+  expect(dummyWrapper.find('input').find({ prop: 'onChange' })).toMatchSnapshot();
+  console.log(dummyWrapper.find('input').find({ prop: 'onChange' }));
+
+  dummyWrapper.find('input').simulate('change',{ target: { value:'art' } });
+  expect(dummyWrapper.instance().handleChange).toHaveBeenCalled();
+});
